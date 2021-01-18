400a1cbeb120129d1830b74a56aa45ba07511f80
Ali Noureddine <ali.nooreddine@mobileartsme.com>
2021-01-18 12:29:24 +0200

    initial commit

--- app/src/main/java/com/mag/dream/MainNativeContent.java
+++ app/src/main/java/com/mag/dream/MainNativeContent.java
@@ -1,9 +1,11 @@
 package com.mag.dream;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.mag.general.GeneralBaseActivity;
+import com.video.glitcheffect.GlitchEffectPlugin;
 
 public class MainNativeContent extends GeneralBaseActivity {
 
@@ -13,6 +15,14 @@ public class MainNativeContent extends GeneralBaseActivity {
         setContentView(R.layout.activity_main_native_content);
         Button portal_btn = findViewById(R.id.portal_btn);
         registerPButton(portal_btn);
+
+        Button glitch_video = findViewById(R.id.glitch_video);
+        glitch_video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GlitchEffectPlugin.getInstance().launch(MainNativeContent.this);
+            }
+        });
 
     }
 }
